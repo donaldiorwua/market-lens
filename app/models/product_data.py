b0df778de5c387fee444cf0143f0f398a354d88e
donaldiorwua <donaldiorwua@gmail.com>
2026-09-17 19:21:27 +0100

    Added more structures for model collection

--- app/models/product_data.py
+++ app/models/product_data.py
@@ -3,6 +3,11 @@ from decimal import Decimal
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 
+
+
+def utc_now():
+    return datetime.now(timezone.utc)
+
 class RawProductData(BaseModel):
     product_name: str
     price:   Decimal
@@ -10,10 +15,7 @@ class RawProductData(BaseModel):
     location: str
     source: str
     availability: bool
-    scraped_at: datetime
-
-def utc_now():
-    return datetime.now(timezone.utc)
+    scraped_at: datetime = Field(default_factory=utc_now)
 
 class ProductData(BaseModel):
     product_id: UUID = Field(default_factory=uuid4)
@@ -23,4 +25,4 @@ class ProductData(BaseModel):
     location: str
     source: str
     availability: bool
-    scraped_at: datetime = Field(default_factory=utc_now)
+    scraped_at: datetime
